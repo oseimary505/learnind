c2d9d03835fc7a5c5f45b600e86d15ee55666ba5
mosei <oseimary505@gmail.com>
2023-04-14 16:06:43 +0000

    added html script

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 i am glad to be here today
 today is easter sunday
 Take a trip into an upgraded, more organized inbox with Yahoo Mail.
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
